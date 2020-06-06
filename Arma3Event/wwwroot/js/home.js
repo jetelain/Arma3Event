8c910f660d45558d6835c8b55d29aa668cf764b2
Julien <julien.etelain@outlook.com>
2020-06-06 16:33:32 +0200

    Test ajout horloge UTC (#6) et indicateur mission (#6)

---
+++ Arma3Event/wwwroot/js/home.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    function fixed(num) {
+        var txt = '00' + num.toString();
+        return txt.substr(txt.length - 2);
+    }
+
+    function updateClock() {
+        var dt = new Date();
+        $('#clock').text(fixed(dt.getUTCHours()) + ':' + fixed(dt.getUTCMinutes()) + ':' + fixed(dt.getUTCSeconds()));
+    }
+
+    updateClock();
+
+    setInterval(updateClock, 1000);
+});
